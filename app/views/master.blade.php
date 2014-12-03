2d1c61ed09734e57094828210cb78cf9686a048d
Matthias Verhoeven <matthiasverhoeven@gmail.com>
2014-12-03 14:51:11 +0100

    Stijl shizzle

---
+++ app/views/master.blade.php
@@ -0,0 +1,40 @@
+<?php
+function setActive($route, $class = 'active') {
+return (Route::current()->uri() == $route) ? $class : '';
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>
+		@yield('title')
+	</title>
+	{{HTML::style("css/main.css")}}
+</head>
+<body>
+
+<div class="navbar">
+<a class="navbar-brand" href="/">{{ HTML::image('assets/images/logo.png', 'alt-text') }}</a>
+  <ul class="nav nav-pills" multilinks-noscroll="true">
+    <li class="active" role="presentation" multilinks-noscroll="true">
+      <a href="#" multilinks-noscroll="true">Reserveren</a>
+    </li>
+    <li role="presentation" multilinks-noscroll="true">
+      <a href="#" multilinks-noscroll="true">Mijn Reservaties</a>
+    </li>
+    <li role="presentation">
+      <a href="#">Materiaal Uitchecken</a>
+    </li>
+    <li role="presentation">
+      <a href="#">Materiaal Inchecken</a>
+    </li>
+  </ul>
+</div>
+
+	<div class="container">
+		@yield('content')	
+	</div>
+</body>
+</html>
+
