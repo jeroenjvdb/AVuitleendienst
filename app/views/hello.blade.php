ee1c81bd048324309455e47e5e8265c01b7e8322
Matthias Verhoeven <matthiasverhoeven@gmail.com>
2014-12-10 13:52:32 +0100

    Inlogkader

---
+++ app/views/hello.blade.php
@@ -0,0 +1,8 @@
+@extends('master')
+@section("title")
+	Uitleendienst
+@stop
+
+@section("content")
+<h1>Materiaaloverzicht</h1>
+@stop
--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -5,19 +5,23 @@
 @stop
 
 @section("content")
-	<h1>Inloggen</h1>
-	@if(Session::has('err'))
-		<div>{{ Session::get('err') }}</div>
-    @endif
-	{{Form::open(['route' => 'sessions.store'])}}
-
-	{{Form::label('email','Email:')}}
-	{{Form::email('email','', array('required' => 'required'))}}
-
-	{{Form::label('password','Password:')}}
-	{{Form::password('password','', array('required' => 'required'))}}
-
-	{{Form::submit('Inloggen')}}
-
-	{{Form::close()}}
+<div id="wrapper">
+    <div id="login" class="animate form">
+    	{{Form::open(['route' => 'sessions.store'])}}
+        <h1>Inloggen</h1> 
+		@if(Session::has('err'))
+			<div>{{ Session::get('err') }}</div>
+	    @endif
+        <p>
+			{{Form::email('email','', array('placeholder' => 'Email','required' => 'required'))}}
+        </p>
+        <p>
+			{{ Form::password('password', ['placeholder' => 'Paswoord','type' => 'password','required','min' => '8']) }}                
+        </p>
+        <p class="login button"> 
+        	{{ Form::submit('Log in', ['class' => 'inloggen','value' => 'LOGIN'])}}
+		</p>
+    	{{ Form::close() }}               
+	</div>
+</div>
 @stop
