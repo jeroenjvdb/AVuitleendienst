d7163fb69d8abcfa5d86708fad90e54b2805811a
Mr1048576 <wouter_44@hotmail.com>
2014-12-17 14:33:19 +0100

    Editing Detail page

--- app/views/materials/detail.blade.php
+++ app/views/materials/detail.blade.php
@@ -9,21 +9,61 @@
 @stop
 
 @section("content")
-	<h2>{{{$material->name}}}</h2>
-	<img src="/images/{{$material->image}}" alt="">
-	<p>{{{$material->details}}}</p>
+
+	<h1>{{{$material->name}}}</h1>
+	<span class="detailavailability">Beschikbaarheid</span>
+	<button class="btn">Reserveren</button>
+	<div class="well well-sm col-sm-12">
+		<div class="col-sm-6">
+			<img src="/images/{{$material->image}}" alt="">
+		</div>
+		<div class="col-sm-6">
+			<p>{{{$material->details}}}</p>
+		</div>
+		<h2 class="indexacctitle">Accessoires</h2>
+		<div class="col-sm-12">
+		@forelse($material->accessories as $accessorie)
+			
+			<div class="col-sm-2">
+				<h3>{{{$accessorie->name}}}</h3>
+				<img src="/images/{{$accessorie->image}}" alt="">
+				<p>{{{$accessorie->details}}}</p>
+			</div>
+
+		@empty
+			<p>Geen accessoires voor dit item.</p>
+		@endforelse
+
+		<h2>Andere suggesties</h2>
+		@foreach($material->categories as $categorie)
+			@forelse($categorie->materials as $catMaterial)
+				@if($material->id != $catMaterial->id)
+				<h3> {{{$catMaterial->name}}}</h3>
+				<img src="/images/{{$catMaterial->image}}" alt="">
+				<p>{{{$catMaterial->details}}}</p>
+				@endif
+			@empty
+			<p>Geen gerelateerde producten gevonden</p>
+			@endforelse
+		@endforeach
+		</div>
+	</div>
+
+
+	<!-- <h2>{{{$material->name}}}</h2>
 	
-	<h2>accessoires</h2>
+	
+	
+	<h2>Accessoires</h2>
 	@forelse($material->accessories as $accessorie)
 	<a href="{{$app['url']->to('/')}}/materials/{{$accessorie->id}}">
 		<div>
-			<h3> {{{$accessorie->name}}}</h3>
-			<img src="/images/{{$accessorie->image}}" alt="">
-			<p>{{{$accessorie->details}}}</p>	
+			
+				
 		</div>
 	</a>	
 	@empty
-		<p>er zijn geen accesores voor dit object.</p>
+		<p>Er zijn geen accesores voor dit object.</p>
 	@endforelse
 	<h2>Andere sugesties</h2>
 	@foreach($material->categories as $categorie)
@@ -36,5 +76,5 @@
 		@empty
 		<p>Geen gerelateerde producten gevonden</p>
 		@endforelse
-	@endforeach
+	@endforeach -->
 @stop
