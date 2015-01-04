fe35b02635c010fab233545b30c7ec24b82b9b18
Mr1048576 <wouter_44@hotmail.com>
2015-01-04 02:24:34 +0100

    logbook styling

--- app/views/materials/logbook.blade.php
+++ app/views/materials/logbook.blade.php
@@ -9,34 +9,56 @@
 @stop
 
 @section("content")
+
+<h1 class="logtitle">Logboek</h1>
 	<div>
-		{{Form::open(['url' => 'logbook/search'])}}
-			<div>
-				{{Form::text('search',Session::has('input') ? Session::get('input')['search'] :'',array('placeholder' => 'materiaal zoeken'))}}
-				{{Form::label('status','Status:')}}
-				{{Form::select('status',array('all' => 'alle','ok' =>'ok' , 'missing' => 'missing' , 'broken' => 'broken'),Session::has('input') ? Session::get('input')['status'] :'all')}}
-				{{Form::label('beschikbaarheid','Beschikbaarheid:')}}
-				{{Form::select('availability',array('all' => 'alle' ,'beschikbaar' => 'beschikbaar' ,'uitgeleend' => 'uitgeleend'),Session::has('input') ? Session::get('availability')['search'] :'all')}}
-				{{Form::label('categorie','Categorie:')}}
-				{{Form::select('categorie',$categories,Session::has('input') ? Session::get('input')['categorie'] :'all')}}
-				{{Form::submit('zoek')}}
+		{{Form::open(['url' => 'logbook/search', "class" => "form"])}}
+			<div class="form-group">
+				<div class="col-md-6 col-sm-6 col-xs-12">
+					{{Form::label('search','Op naam:', ["class" => "control-label"])}}
+					{{Form::text('search',Session::has('input') ? Session::get('input')['search'] :'',array('placeholder' => 'materiaal zoeken', "class" => "form-control"))}}
+
+					{{Form::label('categorie','Categorie:', ["class" => "control-label"])}}
+					{{Form::select('categorie',$categories,Session::has('input') ? Session::get('input')['categorie'] :'all', ["class" => "form-control"])}}
+				</div>
+				
+
+				<div class="col-md-6 col-sm-6 col-xs-12">
+					{{Form::label('status','Status:', ["class" => "control-label"])}}
+					{{Form::select('status',array('all' => 'alle','ok' =>'ok' , 'missing' => 'missing' , 'broken' => 'broken', ),Session::has('input') ? Session::get('input')['status'] :'all', ["class" => "form-control"])}}
+
+
+					{{Form::label('beschikbaarheid','Beschikbaarheid:', ["class" => "control-label"])}}
+					{{Form::select('availability',array('all' => 'alle' ,'beschikbaar' => 'beschikbaar' ,'uitgeleend' => 'uitgeleend', ),Session::has('input') ? Session::get('availability')['search'] :'all', ["class" => "form-control"])}}
+				</div>
+				<div class="col-md-12 col-sm-12 col-xs-12 logsearchbtn">
+					{{Form::submit('Zoek', ["class" => "btn btn-submit btn-info"])}}
+				</div>
 			</div>
 		{{Form::close()}}
 	</div>
-	<h1>Logboek</h1>
+
+	
 	<div>
-		<table>
+
+		<table class="table">
 			<tr>
 				<th>Naam</th>
 				<th>Afbeelding</th>
 				<th>Status</th>
-				<th>Beschibaarheid</th>
+				<th>Beschikbaarheid</th>
 				<th>Barcode</th>
 			</tr>
 		@foreach($logbook as $material)
-			<tr>
+			@if ($material->status == "broken")
+					<tr class="danger">
+			@elseif ($material->status == "missing")
+					<tr class="warning">
+			@else
+				<tr>
+			@endif
 				<td>{{link_to('logbook/'.$material->id,$material->name)}}</td>
-				<td><img src="/images/{{$material->image}}" alt=""></td>
+				<td><img src="/images/{{$material->image}}" alt="{{$material->name}}"></td>
 				<td>{{{$material->status}}}</td>
 				<td>{{{$material->availability}}}</td>
 				<td>{{{$material->barcode}}}</td>
