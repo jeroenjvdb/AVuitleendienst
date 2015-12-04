992ed39aae70e995ae4cb737ce816318550070d1
jeroenjvdb <jeroen_vdb1@hotmail.com>
2015-12-04 09:44:40 +0100

    config

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'avuitleendienst',
 			'username'  => 'root',
-			'password'  => 'number4',
+			'password'  => 'root',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
