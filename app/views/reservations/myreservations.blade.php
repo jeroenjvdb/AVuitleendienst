12dfd0c2959a1dd455b97a87516a5cb0655fa025
Didier De Maeyer <didierdemaeyer@telenet.be>
2015-12-18 01:53:14 +0100

    added FooTable to myreservations view

--- app/views/reservations/myreservations.blade.php
+++ app/views/reservations/myreservations.blade.php
@@ -12,55 +12,61 @@
 	<div class="title">
 		<h1>Mijn reservaties</h1>
 	</div>
-	@forelse($reservations as $reservation)
-	<div class="span4Container">
-		<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 span4">
-			<div class="thumbnail tnDate">
-				<a href="#" class="item">
-					<h3>{{$reservation->name}}</h3>
-					<img src="/images/{{$reservation->image}}" alt="">
-				</a>
-				<p>{{$reservation->reason}}</p>
-				<p class="infogreen">Begin: {{$reservation->begin}}</p>
-				<p class="infored">Einde: {{$reservation->end}}</p>
-			</div>
-		</div>
-		<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 span4">
-			<div class="thumbnail tnDate">
-				<a href="#" class="item">
-					<h3>{{$reservation->name}}</h3>
-					<img src="/images/{{$reservation->image}}" alt="">
-				</a>
-				<p>{{$reservation->reason}}</p>
-				<p class="infogreen">Begin: {{$reservation->begin}}</p>
-				<p class="infored">Einde: {{$reservation->end}}</p>
-			</div>
-		</div>
-		<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 span4">
-			<div class="thumbnail tnDate">
-				<a href="#" class="item">
-					<h3>{{$reservation->name}}</h3>
-					<img src="/images/{{$reservation->image}}" alt="">
-				</a>
-				<p>{{$reservation->reason}}</p>
-				<p class="infogreen">Begin: {{$reservation->begin}}</p>
-				<p class="infored">Einde: {{$reservation->end}}</p>
-			</div>
-		</div>
-		<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12 span4">
-			<div class="thumbnail tnDate">
-				<a href="#" class="item">
-					<h3>{{$reservation->name}}</h3>
-					<img src="/images/{{$reservation->image}}" alt="">
-				</a>
-				<p>{{$reservation->reason}}</p>
-				<p class="infogreen">Begin: {{$reservation->begin}}</p>
-				<p class="infored">Einde: {{$reservation->end}}</p>
+
+	<!-- Search table -->
+	<div class="panel-body">
+		<div class="form-group">
+			<div class="input-group">
+				<input id="filter" type="text" typeahead="state for state in states | filter:$viewValue | limitTo:8" class="form-control input-sm bg-light no-border rounded padder" placeholder="Zoek...">
+				<span class="input-group-btn heading-button">
+					<button type="submit" class="btn btn-lg bg-light rounded"><i class="fa fa-search"></i></button>
+				</span>
 			</div>
 		</div>
 	</div>
-		
-	@empty
-		<h4 class="notification">U heeft op dit moment nog geen reservaties geplaatst.</h4>
-	@endforelse
+
+	@if(count($reservations) > 0)
+		<table class="footable table m-b-none toggle-arrow" ui-jq="footable" data-filter="#filter" data-page-size="10">
+			<thead>
+				<tr>
+					<th>Item</th>
+					<th data-hide="phone" data-sort-initial="true">Van</th>
+					<th data-hide="phone">Tot</th>
+					<th data-hide="all">Reden</th>
+					<th data-hide="all">Foto</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach ($reservations as $reservation)
+					<tr>
+						<td>{{$reservation->name}}</td>
+						<td>{{ date('Y/m/d - H:i', strtotime($reservation->begin)) }}</td>
+						<td>{{ date('Y/m/d - H:i', strtotime($reservation->end)) }}</td>
+						<td>{{$reservation->reason}}</td>
+						<td><img src="/images/{{$reservation->image}}" alt=""></td>
+					</tr>
+				@endforeach
+			</tbody>
+
+			<!-- Table footer -->
+			<tfoot class="hide-if-no-paging">
+				<tr>
+					<td colspan="6" class="text-center">
+						<ul class="pagination"></ul>
+					</td>
+				</tr>
+			</tfoot>
+		</table>
+	@else
+		<h4 class="notification">Je hebt geen openstaande reservaties.</h4>
+	@endif
+
+@stop
+
+@section('scripts')
+	<script type="text/javascript">
+		$(function () {
+			$('.footable').footable();
+		});
+	</script>
 @stop
