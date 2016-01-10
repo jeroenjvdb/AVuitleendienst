fdf99ae4a3b41519b9b1f281fecf20a1f3ddf4d5
jeroen Van den Broeck <jeroen_vdb1@hotmail.com>
2016-01-10 10:01:48 +0100

    edit notifications

--- app/views/notifications/dashboard.blade.php
+++ app/views/notifications/dashboard.blade.php
@@ -9,7 +9,15 @@ dashboard
 @stop
 
 @section('content')
+
 	@foreach($notifications as $notification)
+	<div class="col-md-10">
 		<p>{{ $notification->message }}</p>
+	</div>
+	<div class="col-md-2">
+		@if(Auth::user() && Auth::user()->type == "admin")
+			<a href="{{ route('notifications.edit', ['id' => $notification->id]) }}">edit</a>
+		@endif
+	</div>
 	@endforeach
 @stop
